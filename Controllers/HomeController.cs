21b86aae2183aa628413fb58a1db63dfb5ac7625
namlih <hasanburaknamli@gmail.com>
2017-12-07 21:25:26 +0300

    Added timeline feature to home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,16 +5,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HomeBrewing.Models;
+using HomeBrewing.Models.SubscriptionViewModels;
+using HomeBrewing.Models.RecipeViewModels;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace HomeBrewing.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _userManager = userManager;
         }
 
+        public IActionResult Index()
+        {
+            using (var db = new DatabaseContext())
+            {
+                var subscribedUsersInfo = db.Subscription.Where(s => s.FollowerUserID == _userManager.GetUserId(User)).ToList();
+                var RecipesInfo = new List<List<CreateRecipeViewModel>>();
+                foreach (var item in subscribedUsersInfo)
+                {
+                    var recipes = db.Recipe.Where(r => r.UserId == item.FollowedUserID).ToList();
+                    RecipesInfo.Add(recipes);
+                }
+                ViewBag.Recipes = RecipesInfo;
+                return View();
+            }
+        }
 
 
         public IActionResult About()
